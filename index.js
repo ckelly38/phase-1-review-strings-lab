848f765c952ea0ed1156ca421c69d5287fda4b60
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-05 22:23:02 -0600

    review strings lab completed from SEPREP

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Grace Hopper";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
